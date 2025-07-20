a16c9b2406011bab42eed32daba347e6d21667a0
kevin-ai-04 <117845901+kevin-ai-04@users.noreply.github.com>
2025-07-20 15:46:06 +0530

    bug fix

--- build/web/night-trucker.service.worker.js
+++ build/web/night-trucker.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753005416|3691490117';
+const CACHE_VERSION = '1753006507|4781990845';
 /** @type {string} */
 const CACHE_PREFIX = 'night-trucker-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
